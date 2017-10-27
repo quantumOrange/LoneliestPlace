e671c6f54c54d9590c040f57cd94e4ca8759785a
David Crooks <crodave@gmail.com>
2017-10-27 12:42:04 +0200

    Refactored PlaceCollection

---
+++ LoneliestPlace/PlacesCollection.hpp
@@ -0,0 +1,51 @@
+//
+//  PlacesCollection.hpp
+//  LoneliestPlace
+//
+//  Created by David Crooks on 27/10/2017.
+//  Copyright © 2017 David Crooks. All rights reserved.
+//
+
+#ifndef PlacesCollection_hpp
+#define PlacesCollection_hpp
+
+#include <stdio.h>
+
+#include <fstream>
+#include <vector>
+#include <ctime>
+//#include <string>
+#include "nanoflann.hpp"
+using namespace std;
+using namespace nanoflann;
+
+struct Place {
+    int x,y;
+    string name;
+};
+
+struct PlaceCollection
+{
+    vector<Place> places;
+    
+    PlaceCollection(ifstream &inputFile);
+    
+    Place mostIsolated();
+    
+    /* Adaptor interface for nanaoflann k-d tree  */
+    
+    inline size_t kdtree_get_point_count() const { return places.size(); }
+    
+    inline double kdtree_get_pt(const size_t idx, int dim) const
+    {
+        if (dim == 0) return double(places[idx].x);
+        else return double(places[idx].y);
+    }
+    
+    template <class BBOX>
+    bool kdtree_get_bbox(BBOX& /*bb*/) const { return false; }
+};
+
+
+
+#endif /* PlacesCollection_hpp */
--- LoneliestPlace/main.cpp
+++ LoneliestPlace/main.cpp
@@ -12,86 +12,10 @@
 #include <ctime>
 //#include <string>
 #include "nanoflann.hpp"
+#include "PlacesCollection.hpp"
+
 using namespace std;
 using namespace nanoflann;
-
-struct Place {
-    int x,y;
-    string name;
-};
-
-struct PlaceCollection
-{
-    vector<Place> places;
-    
-    PlaceCollection(ifstream &inputFile){
-        Place p;
-        
-        while (inputFile >> p.name) {
-            
-            inputFile >> p.x;
-            inputFile >> p.y;
-            
-            places.push_back(p);
-        }
-    }
-    
-    Place mostIsolated(){
-        //Create a k-d tree
-        const int dim = 2;
-        
-        typedef KDTreeSingleIndexAdaptor<
-        L2_Simple_Adaptor<double, PlaceCollection> ,
-        PlaceCollection,
-        dim
-        > KD_tree_t;
-        
-        KD_tree_t   tree(dim, *this, KDTreeSingleIndexAdaptorParams(10 /* max leaf */) );
-        tree.buildIndex();
-        
-        const size_t num_results = 2;
-        size_t ret_index[2];
-        double out_dist_sqr[2];
-        nanoflann::KNNResultSet<double> resultSet(num_results);
-        resultSet.init(ret_index, out_dist_sqr );
-        
-        double maxDistanceSq = 0;
-
-        Place  mostIsolatedPlace;
-        
-        //find nearest neighbour for each place, saving that place if it is the most isolated so far.
-        for(auto place: places){
-            
-            resultSet.init(ret_index, out_dist_sqr );
-            ///create a test point from the current place
-            double query_pt[dim] = {double(place.x) , double(place.y)};
-            //find the two nearest neighbours for the test point. The first should be the current place itself, the second will be its nearest neighbour.
-            tree.findNeighbors(resultSet, &query_pt[0], nanoflann::SearchParams(10));
-            
-            if (out_dist_sqr[1] > maxDistanceSq ) {
-                //The current place is the most isolated so far.
-                maxDistanceSq = out_dist_sqr[1];
-                mostIsolatedPlace = place;
-            }
-        }
-        
-        return mostIsolatedPlace;
-    };
-    
-    /* Adaptor interface for nanaoflann k-d tree  */
-    
-    inline size_t kdtree_get_point_count() const { return places.size(); }
-    
-    inline double kdtree_get_pt(const size_t idx, int dim) const
-    {
-        if (dim == 0) return double(places[idx].x);
-        else return double(places[idx].y);
-    }
-    
-    template <class BBOX>
-    bool kdtree_get_bbox(BBOX& /*bb*/) const { return false; }
-};
-
 
 int main(int argc, const char * argv[]) {
   
