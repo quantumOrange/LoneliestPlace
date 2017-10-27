73bba8cf6318586646b5ae2ec7d5240eb00dc041
David Crooks <crodave@gmail.com>
2017-10-27 11:09:44 +0200

    Read place data from file into struct

--- LoneliestPlace/main.cpp
+++ LoneliestPlace/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
 
+#include "nanoflann.hpp"
+using namespace std;
+using namespace nanoflann;
+
+struct Place {
+    int x,y;
+    string name;
+};
+
+vector<Place> places;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    ifstream inputFile;
+    
+
+    inputFile.open("/Users/decrooks/Developer/WRLD/cpp/LoneliestPlace/LoneliestPlace/problem_small.txt");
+    
+    if (!inputFile) {
+        cerr << "Unable to open file";
+        exit(1);   // call system to stop
+    }
+    
+    Place p;
+    
+    cout << "Reading data...";
+    
+    while (inputFile >> p.name) {
+        
+        inputFile >> p.x;
+        inputFile >> p.y;
+        
+        
+        places.push_back(p);
+    }
+    
+    cout << "done" << endl;
+    
     return 0;
 }
