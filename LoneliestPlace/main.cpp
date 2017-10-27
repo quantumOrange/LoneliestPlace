a186f9350a729290ffc4aebb6090826466a3ee45
David Crooks <crodave@gmail.com>
2017-10-27 10:48:56 +0200

    Initial Commit

---
+++ LoneliestPlace/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LoneliestPlace
+//
+//  Created by David Crooks on 27/10/2017.
+//  Copyright © 2017 David Crooks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
