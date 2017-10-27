00cc7255b9578f7a8a7da317862380307601ea81
David Crooks <crodave@gmail.com>
2017-10-27 11:53:12 +0200

    refactored find most isolated

--- LoneliestPlace/main.cpp
+++ LoneliestPlace/main.cpp
@@ -36,6 +36,49 @@ struct PlaceCollection
         }
     }
     
+    Place mostIsolated(){
+        //Create a k-d tree
+        const int dim = 2;
+        
+        typedef KDTreeSingleIndexAdaptor<
+        L2_Simple_Adaptor<double, PlaceCollection> ,
+        PlaceCollection,
+        dim
+        > KD_tree_t;
+        
+        
+        KD_tree_t   tree(dim, *this, KDTreeSingleIndexAdaptorParams(10 /* max leaf */) );
+        tree.buildIndex();
+        
+        const size_t num_results = 2;
+        size_t ret_index[2];
+        double out_dist_sqr[2];
+        nanoflann::KNNResultSet<double> resultSet(num_results);
+        resultSet.init(ret_index, out_dist_sqr );
+        
+        
+        double maxDistanceSq = 0;
+        //int mostIsolatedIndex = side;
+        Place  mostIsolatedPlace;
+        
+        for(auto place: places){
+            
+            resultSet.init(ret_index, out_dist_sqr );
+            double query_pt[dim] = {double(place.x) , double(place.y)};
+            tree.findNeighbors(resultSet, &query_pt[0], nanoflann::SearchParams(10));
+            
+            if (out_dist_sqr[1] > maxDistanceSq ) {
+
+                maxDistanceSq = out_dist_sqr[1];
+                mostIsolatedPlace = place;
+                
+            }
+        }
+        
+        return mostIsolatedPlace;
+        
+    };
+    
     /* Adaptor interface for nanaoflann k-d tree  */
     
     inline size_t kdtree_get_point_count() const { return places.size(); }
@@ -64,50 +107,7 @@ int main(int argc, const char * argv[]) {
     
     auto placeCollection = PlaceCollection(inputFile);
     
-    for(auto p:placeCollection.places){
-        cout << p.name << endl;
-    }
-    
-    //Create a k-d tree
-    const int dim = 2;
-    
-    typedef KDTreeSingleIndexAdaptor<
-    L2_Simple_Adaptor<double, PlaceCollection> ,
-    PlaceCollection,
-    dim
-    > KD_tree_t;
-    
-    
-    KD_tree_t   tree(dim, placeCollection, KDTreeSingleIndexAdaptorParams(10 /* max leaf */) );
-    tree.buildIndex();
-    
-    const size_t num_results = 2;
-    size_t ret_index[2];
-    double out_dist_sqr[2];
-    nanoflann::KNNResultSet<double> resultSet(num_results);
-    resultSet.init(ret_index, out_dist_sqr );
-    
-    
-    double maxDistanceSq = 0;
-    //int mostIsolatedIndex = side;
-    Place  mostIsolatedPlace;
-    
-    for(auto place: placeCollection.places){
-        
-        resultSet.init(ret_index, out_dist_sqr );
-        double query_pt[dim] = {double(place.x) , double(place.y)};
-        tree.findNeighbors(resultSet, &query_pt[0], nanoflann::SearchParams(10));
-        
-        // cout << place.name << " -> " << "place" <<ret_index[1] << "  d = " << out_dist_sqr[1] <<endl;
-        
-        if (out_dist_sqr[1] > maxDistanceSq ) {
-            
-            //std::cout << " !!!!!!!!" << out_dist_sqr[1] << endl;
-            maxDistanceSq = out_dist_sqr[1];
-            mostIsolatedPlace = place;
-            
-        }
-    }
+    Place mostIsolatedPlace = placeCollection.mostIsolated();
     
     std::cout << "Most Isolated Place is: " << mostIsolatedPlace.name << endl;
     
