8c22499ac059ea558879076eb8661b9154a52627
David Crooks <crodave@gmail.com>
2017-10-27 11:31:00 +0200

    created placeCollection struct to interface with k-d tree. Refactored reading data into places vector.

--- LoneliestPlace/main.cpp
+++ LoneliestPlace/main.cpp
@@ -21,11 +21,42 @@ struct Place {
 };
 
 vector<Place> places;
+
+struct PlaceCollection
+{
+    vector<Place> places;
+    
+    PlaceCollection(ifstream &inputFile){
+        Place p;
+        
+        while (inputFile >> p.name) {
+            
+            inputFile >> p.x;
+            inputFile >> p.y;
+            
+            places.push_back(p);
+        }
+    }
+    
+    /* Adaptor interface for nanaoflann k-d tree  */
+    
+    inline size_t kdtree_get_point_count() const { return places.size(); }
+    
+    inline double kdtree_get_pt(const size_t idx, int dim) const
+    {
+        if (dim == 0) return double(places[idx].x);
+        else return double(places[idx].y);
+    }
+    
+    template <class BBOX>
+    bool kdtree_get_bbox(BBOX& /*bb*/) const { return false; }
+};
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     ifstream inputFile;
     
-
     inputFile.open("/Users/decrooks/Developer/WRLD/cpp/LoneliestPlace/LoneliestPlace/problem_small.txt");
     
     if (!inputFile) {
@@ -33,17 +64,10 @@ int main(int argc, const char * argv[]) {
         exit(1);   // call system to stop
     }
     
-    Place p;
+    auto placeCollection = PlaceCollection(inputFile);
     
-    cout << "Reading data...";
-    
-    while (inputFile >> p.name) {
-        
-        inputFile >> p.x;
-        inputFile >> p.y;
-        
-        
-        places.push_back(p);
+    for(auto p:placeCollection.places){
+        cout << p.name << endl;
     }
     
     cout << "done" << endl;
